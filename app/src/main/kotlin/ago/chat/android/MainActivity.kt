663f62d6572b569ba5734c56b651ad19592abd64
Andrey Golyakov <golyakoff@yandex.ru>
2026-09-22 01:30:54 +0300

    test(26-08 probe 3/3): deliberate ktlint violation, to be reverted
    Proving CI fails fast at the format-check step, before lint/test/build
    even run, per this item's own Done-when. Reverted in the next commit.

--- app/src/main/kotlin/ago/chat/android/MainActivity.kt
+++ app/src/main/kotlin/ago/chat/android/MainActivity.kt
@@ -1,7 +1,7 @@
 package ago.chat.android
 
-import ago.chat.android.core.domain.shortId
 import android.os.Bundle
+import ago.chat.android.core.domain.shortId
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
