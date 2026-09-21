ff031afe97ebb1f6baf09c9f9cd6c249db5ba02b
Andrey Golyakov <golyakoff@yandex.ru>
2026-09-22 01:34:16 +0300

    Revert "test(26-08 probe 3/3): deliberate ktlint violation, to be reverted"
    This reverts commit 663f62d6572b569ba5734c56b651ad19592abd64.

--- app/src/main/kotlin/ago/chat/android/MainActivity.kt
+++ app/src/main/kotlin/ago/chat/android/MainActivity.kt
@@ -1,7 +1,7 @@
 package ago.chat.android
 
-import android.os.Bundle
 import ago.chat.android.core.domain.shortId
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
